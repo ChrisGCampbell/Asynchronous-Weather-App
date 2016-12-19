1d271a9ad28a7cf36012c3354c9b4c342f41f97b
ChrisGCampbell <chris@chrisgcampbell.com>
2016-12-18 21:28:36 -0500

    added error handling

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 /**
  * Created by Chris-Campbell on 12/18/2016.
  */
-const request = require('request');
+const yargs = require('yargs');
 
-request({
-    url:'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
-    json: true
-},(error, response, body) => {
+const geocode = require('./geocode/geocode');
 
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].formatted_address}`);
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
 
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2));
+}
 });
