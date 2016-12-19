1f784c5aa9a76bbfc5c17db74ed73d2abfaba1ba
ChrisGCampbell <chris@chrisgcampbell.com>
2016-12-18 22:12:51 -0500

    added api configuration settings

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -13,16 +13,15 @@ var geocodeAddress = (address, callback) => {
         if (error) {
             callback('Unable to connect to Google servers.');
         } else if (body.status === 'ZERO_RESULTS') {
-        callback('Unable to find that address.');
-    } else if (body.status === 'OK') {
-        callback(undefined, {
-            address: body.results[0].formatted_address,
-            latitude: body.results[0].geometry.location.lat,
-            longitude: body.results[0].geometry.location.lng
-        });
-    }
-});
+            callback('Unable to find that address.');
+        } else if (body.status === 'OK') {
+            callback(undefined, {
+                address: body.results[0].formatted_address,
+                latitude: body.results[0].geometry.location.lat,
+                longitude: body.results[0].geometry.location.lng
+            });
+        }
+    });
 };
 
 module.exports.geocodeAddress = geocodeAddress;
-
